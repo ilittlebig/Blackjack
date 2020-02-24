7052b3a5cd5e01f9a33483c0bdeb5c996d227a93
ilittlebig <fagelbuse@hotmail.se>
2020-02-24 17:18:49 +0100

    Implemented methods to the Hand class; AddCard and GetCards. Added unit tests to test these 2 new methods as well

--- Hand/Hand.h
+++ Hand/Hand.h
@@ -1,11 +1,15 @@
 #ifndef HAND_H_INCLUDED
 #define HAND_H_INCLUDED
 
+#include <vector>
 #include "../Card/Card.h"
 
 class Hand {
 public:
 	void AddCard(Card card);
+	std::vector<Card> GetCards();
+private:
+	std::vector<Card> cards;
 };
 
 #endif // !HAND_H_INCLUDED
