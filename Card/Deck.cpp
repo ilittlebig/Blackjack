621d8b6e928a417037275a7ec62ea984260be3e3
ilittlebig <fagelbuse@hotmail.se>
2020-02-24 20:47:54 +0100

    Implemented two new methods to the Deck class; GetRandomSuit and GetRandomValue. These two methods are private, but when you call GetTopCard these two methods will generate a suit and a value which will get returned to the caller

--- Card/Deck.cpp
+++ Card/Deck.cpp
@@ -1,6 +1,25 @@
 #include "Deck.h"
 
+std::string Deck::GetRandomSuit() {
+	srand(std::random_device()());
+	std::vector<std::string> types = { "C", "S", "H", "D", "K", "Q" };
+	return types[rand() % 6];
+}
+
+std::string Deck::GetRandomValue() {
+	std::mt19937 rng{std::random_device()()};
+	std::uniform_int_distribution<int> gen(2, 9);
+	return std::to_string(gen(rng));
+}
+
 Card Deck::GetTopCard() {
-	Card *card = new Card("H", "2");
+	std::string suit = GetRandomSuit();
+	std::string value = GetRandomValue();
+
+	if (suit == "K" || suit == "Q") {
+		Card* card = new Card(suit, "10");
+		return *card;
+	}
+	Card* card = new Card(suit, value);
 	return *card;
 }
--- Card/Deck.h
+++ Card/Deck.h
@@ -2,10 +2,15 @@
 #define DECK_H_INCLUDED
 
 #include "Card.h"
+#include <random>
+#include <iomanip>
 
 class Deck {
 public:
 	Card GetTopCard();
+private:
+	std::string GetRandomSuit();
+	std::string GetRandomValue();
 };
 
 #endif // !DECK_H_INCLUDED
